5e8e3d48abb673e28302dca67e3f42e9ca839ce3
Can Toraman <cantoraman@gmail.com>
2018-05-30 11:29:13 +0100

    pub class created

---
+++ specs/pub_spec.rb
@@ -0,0 +1,25 @@
+require ("minitest/autorun")
+require_relative("../pub.rb")
+require_relative("../food.rb")
+require_relative("../drink.rb")
+require_relative("../customer.rb")
+
+
+class PubTest < Minitest::Test
+
+  def setup
+    @pub1 = Pub.new("The Bonnington", 100, [])
+    @drink1= Drink.new("Red Wine", 2, 13)
+    @drink2= Drink.new("Beer", 1, 4)
+    @drink3= Drink.new("Whisky", 5, 40)
+    @drink4= Drink.new("Gin", 4, 20)
+    @pub2 = Pub.new("Standing Order", 200, [@drink1, @drink2, @drink3, @drink4])
+  end
+
+  def test_name
+    assert_equal("The Bonnington", @pub1.name)
+  end
+
+
+
+end
